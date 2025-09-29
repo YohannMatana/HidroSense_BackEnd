ddd5b3f9c273dd504b35db8fc1b3876da0138b53
newGustavo <gustavo.lima@riobranco.ac.gov.br>
2025-09-29 08:28:35 -0500

    adiciona a primeira versão da estatistica

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\UmidadeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Umidade;
@@ -18,5 +19,7 @@ Route::get('/umidade', function () {
 Route::get('/limite', function () {
 
 });
+
+Route::get('/umidades', [UmidadeController::class, 'index'])->name('api.umidades');
 
 Route::post('/set-limite', [MqttController::class, 'setLimite']);
