868036a7c2a64aa3e1425954cf42090743ed98dc
yohann matana <yohann.antunes18@gmail.com>
2025-10-22 13:57:08 -0500

    Update favicon and apple-touch-icon links in app layout
    - Replaced existing favicon and apple-touch-icon links with a new logo image for improved branding consistency.
    - Updated image paths to point to the new logo asset in the resources directory.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,9 +32,9 @@
 
         <title inertia>{{ config('app.name', 'Hidrosense') }}</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/images/tiny-hidrosense-LOGO-nobg-gota.png" sizes="any">
+        <link rel="icon" href="/images/tiny-hidrosense-LOGO-nobg-gota.png" type="image/png">
+        <link rel="apple-touch-icon" href="/images/tiny-hidrosense-LOGO-nobg-gota.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
